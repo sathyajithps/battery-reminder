77a5506349fece77d3d9371e61c06c5c6af3c24e
Arundas Vallath <iam.das.v93@gmail.com>
2021-12-27 17:24:44 +0530

    Added notifications (#1)
    * Test notifications
    
    * Added Notifications Module
    
    Co-authored-by: Sathyajith_P_S <sathyajithsps@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,28 @@
 use battery::units::Ratio;
 use battery::{Manager, State};
 use spin_sleep::sleep;
-use std::time::{Duration, SystemTime};
+use std::time::Duration;
 
+mod notification;
+
+use notification::{
+    battery_full,battery_low,
+};
 fn main() {
     loop {
         if let Ok(manager) = Manager::new() {
             if let Ok(mut batteries) = manager.batteries() {
                 if let Some(Ok(battery)) = batteries.next() {
-                    if battery.state_of_charge() <= Ratio::from(0.30) {
-                        if battery.state() == State::Discharging {
-                            println!("Charge dummy");
-                        }
-                        if battery.state() == State::Charging {
-                            println!("We good");
-                        }
+                    if battery.state_of_charge() <= Ratio::from(0.30) && battery.state() == State::Discharging{
+                            battery_low::battery_low_notification();
                     }
-                    if battery.state_of_charge() > Ratio::from(0.30)
-                        && battery.state_of_charge() < Ratio::from(0.90)
-                    {
-                        println!("Wee boomin");
-                    }
-                    if battery.state_of_charge() >= Ratio::from(0.90) {
-                        if battery.state() == State::Discharging {
-                            println!("We good");
-                        }
-                        if battery.state() == State::Charging {
-                            println!("Disconnect the charger");
-                        }
+                    if battery.state_of_charge() >= Ratio::from(0.90) && battery.state() == State::Charging{
+                            battery_full::battery_full_notification();
+
                     }
                 }
             }
         }
-        println!("{:?}", SystemTime::now());
         sleep(Duration::from_secs(180));
     }
 }
---
+++ src/notification/battery_full.rs
@@ -0,0 +1,11 @@
+// use notify_rust::Notification;
+use super::Notification;
+
+pub fn battery_full_notification(){
+    Notification::new()
+    .summary("Battery Full")
+    .body("Remove the charger")
+    .icon(format!("{}/assets/battery_full.png", env!("CARGO_MANIFEST_DIR")).as_str())
+    .show()
+    .unwrap();
+}
