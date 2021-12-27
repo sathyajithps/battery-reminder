c21f22019f49bdf11b53f6192191350612444484
Arundas Vallath <iam.das.v93@gmail.com>
2021-12-27 17:20:40 +0530

    added notifications

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,28 @@
-// use battery::units::Ratio;
-// use battery::{Manager, State};
-use notify_rust::Notification;
+use battery::units::Ratio;
+use battery::{Manager, State};
 use spin_sleep::sleep;
-use std::time::{Duration, SystemTime};
+use std::time::Duration;
+
+mod notification;
+
+use notification::{
+    battery_full,battery_low,
+};
 fn main() {
     loop {
-        Notification::new()
-            .summary("Battery Full")
-            .body("Remove the charger")
-            .icon(format!("{}/assets/battery_full.png", env!("CARGO_MANIFEST_DIR")).as_str())
-            .show()
-            .unwrap();
-        // if let Ok(manager) = Manager::new() {
-        //     if let Ok(mut batteries) = manager.batteries() {
-        //         if let Some(Ok(battery)) = batteries.next() {
-        //             if battery.state_of_charge() <= Ratio::from(0.30) {
-        //                 if battery.state() == State::Discharging {
-        //                     println!("Charge dummy");
-        //                 }
-        //                 if battery.state() == State::Charging {
-        //                     println!("We good");
-        //                 }
-        //             }
-        //             if battery.state_of_charge() > Ratio::from(0.30)
-        //                 && battery.state_of_charge() < Ratio::from(0.90)
-        //             {
-        //                 println!("Wee boomin");
-        //             }
-        //             if battery.state_of_charge() >= Ratio::from(0.90) {
-        //                 if battery.state() == State::Discharging {
-        //                     println!("We good");
-        //                 }
-        //                 if battery.state() == State::Charging {
-        //                     println!("Disconnect the charger");
-        //                 }
-        //             }
-        //         }
-        //     }
-        // }
-        // println!("{:?}", SystemTime::now());
+        if let Ok(manager) = Manager::new() {
+            if let Ok(mut batteries) = manager.batteries() {
+                if let Some(Ok(battery)) = batteries.next() {
+                    if battery.state_of_charge() <= Ratio::from(0.30) && battery.state() == State::Discharging{
+                            battery_low::battery_low_notification();
+                    }
+                    if battery.state_of_charge() >= Ratio::from(0.90) && battery.state() == State::Charging{
+                            battery_full::battery_full_notification();
+
+                    }
+                }
+            }
+        }
         sleep(Duration::from_secs(180));
     }
 }
---
+++ src/notification/mod.rs
@@ -0,0 +1,3 @@
+use notify_rust::Notification;
+pub mod battery_full;
+pub mod battery_low;
