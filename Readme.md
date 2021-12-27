b3608f5806d4601394b7effc39e00026bfad4f04
Sathyajith_P_S <sathyajithsps@gmail.com>
2021-12-27 13:39:18 +0530

    Added Readme.md

---
+++ Readme.md
@@ -0,0 +1,15 @@
+# Battery Reminder
+
+## What is it ?
+
+Battery Reminder is our attempt at helping users maintain their battery health by reminding the level when it spills over or under a range.
+
+Current range is : 30% - 90%
+
+## Currently Status
+
+-   Early Development
+
+## How to run
+
+Will update once the core is running
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
+use battery::units::Ratio;
+use battery::{Manager, State};
+use spin_sleep::sleep;
+use std::time::{Duration, SystemTime};
+
 fn main() {
-    println!("Hello, world!");
+    loop {
+        if let Ok(manager) = Manager::new() {
+            if let Ok(mut batteries) = manager.batteries() {
+                if let Some(Ok(battery)) = batteries.next() {
+                    if battery.state_of_charge() <= Ratio::from(0.30) {
+                        if battery.state() == State::Discharging {
+                            println!("Charge dummy");
+                        }
+                        if battery.state() == State::Charging {
+                            println!("We good");
+                        }
+                    }
+                    if battery.state_of_charge() > Ratio::from(0.30)
+                        && battery.state_of_charge() < Ratio::from(0.90)
+                    {
+                        println!("Wee boomin");
+                    }
+                    if battery.state_of_charge() >= Ratio::from(0.90) {
+                        if battery.state() == State::Discharging {
+                            println!("We good");
+                        }
+                        if battery.state() == State::Charging {
+                            println!("Disconnect the charger");
+                        }
+                    }
+                }
+            }
+        }
+        println!("{:?}", SystemTime::now());
+        sleep(Duration::from_secs(180));
+    }
 }
